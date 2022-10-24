e40cb02229b79308da4b1bd66bcc7f256f1af351
Usama Karim <usama@appstree.io>
2022-10-24 18:09:32 +0500

    style: :lipstick: Add gradient in destination screen

--- lib/pages/destination.dart
+++ lib/pages/destination.dart
@@ -21,104 +21,119 @@ class Destination extends StatelessWidget {
             place.image,
             fit: BoxFit.cover,
           ),
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    const SizedBox(height: 48),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                          onPressed: () => Navigator.of(context).pop(),
-                          icon: const Icon(
-                            Icons.arrow_back,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Image.asset(IconsAssets.heart),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          place.name,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        const Spacer(),
-                        Image.asset(IconsAssets.location),
-                        Constants.sizeW08,
-                        Text(
-                          place.country,
-                          style: const TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                    Constants.sizeH24,
-                    Text(
-                      place.description,
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                    Constants.sizeH20,
-                    RatingBar(
-                      place: place,
-                      textColor: Colors.white,
-                      iconColor: Colors.white,
-                    ),
-                    Constants.sizeH20,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '${place.price}/person',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            shape: MaterialStatePropertyAll(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
-                            padding: MaterialStateProperty.all(
-                              const EdgeInsets.symmetric(
-                                horizontal: 24,
-                                vertical: 16,
-                              ),
-                            ),
-                            backgroundColor: MaterialStateProperty.all(
-                              Constants.starIconColor,
+          Container(
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Colors.black.withAlpha(0),
+                  Colors.black12,
+                  Colors.black45,
+                  Colors.black
+                ],
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(height: 48),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            icon: const Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
                             ),
                           ),
-                          child: const Text(
-                            'Book Now',
-                            style: TextStyle(
-                              color: Colors.black,
+                          Image.asset(IconsAssets.heart),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            place.name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          const Spacer(),
+                          Image.asset(IconsAssets.location),
+                          Constants.sizeW08,
+                          Text(
+                            place.country,
+                            style: const TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Constants.sizeH24,
+                      Text(
+                        place.description,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      Constants.sizeH20,
+                      RatingBar(
+                        place: place,
+                        textColor: Colors.white,
+                        iconColor: Colors.white,
+                      ),
+                      Constants.sizeH20,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${place.price}/person',
+                            style: const TextStyle(
+                              color: Colors.white,
                               fontSize: 16,
-                              fontWeight: FontWeight.w600,
                             ),
                           ),
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ],
+                          TextButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              shape: MaterialStatePropertyAll(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              padding: MaterialStateProperty.all(
+                                const EdgeInsets.symmetric(
+                                  horizontal: 24,
+                                  vertical: 16,
+                                ),
+                              ),
+                              backgroundColor: MaterialStateProperty.all(
+                                Constants.starIconColor,
+                              ),
+                            ),
+                            child: const Text(
+                              'Book Now',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
--- lib/widgets/popular_packages.dart
+++ lib/widgets/popular_packages.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vacation_time/models/place.dart';
+import 'package:vacation_time/pages/destination.dart';
 import 'package:vacation_time/utils/constants.dart';
 import 'package:vacation_time/widgets/rating_bar.dart';
 import 'package:vacation_time/widgets/ticker.dart';
@@ -18,63 +19,71 @@ class PopularPackage extends StatelessWidget {
           title: 'Popular Package',
         ),
         ...places
-            .map((place) => Container(
-                  padding: const EdgeInsets.all(16),
-                  margin: const EdgeInsets.only(right: 24, bottom: 24),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(24),
-                    border: Border.all(
-                      color: Constants.borderColor,
+            .map((place) => GestureDetector(
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: ((context) => Destination(place: place)),
                     ),
                   ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(
-                          place.image,
-                          fit: BoxFit.cover,
-                          width: 86,
-                          height: 116,
-                        ),
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    margin: const EdgeInsets.only(right: 24, bottom: 24),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      border: Border.all(
+                        color: Constants.borderColor,
                       ),
-                      Constants.sizeW10,
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          // mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Row(
-                              // mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  place.name,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                Image.asset(IconsAssets.heart)
-                              ],
-                            ),
-                            Constants.sizeH10,
-                            Text(
-                              place.price.toString(),
-                              style: const TextStyle(color: Colors.red),
-                            ),
-                            RatingBar(
-                              place: place,
-                              textColor: Colors.black,
-                              iconColor: Colors.black26,
-                            ),
-                            Text(place.description)
-                          ],
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                            place.image,
+                            fit: BoxFit.cover,
+                            width: 86,
+                            height: 116,
+                          ),
                         ),
-                      )
-                    ],
+                        Constants.sizeW10,
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            // mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Row(
+                                // mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    place.name,
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  Image.asset(IconsAssets.heart)
+                                ],
+                              ),
+                              Constants.sizeH10,
+                              Text(
+                                place.price.toString(),
+                                style: const TextStyle(color: Colors.red),
+                              ),
+                              RatingBar(
+                                place: place,
+                                textColor: Colors.black,
+                                iconColor: Colors.black26,
+                              ),
+                              Text(place.description)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ))
             .toList()
